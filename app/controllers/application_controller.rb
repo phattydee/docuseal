fd2153d98ce4272cdd5bbaecbcd56b105d8900af
phattydee <patrickdunn@bigtimemotionmedia.com>
2025-07-10 15:45:44 -0230

    removed "phonehome"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,9 +39,9 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    if request.domain == 'docuseal.com'
-      return { host: 'docuseal.com', protocol: ENV['FORCE_SSL'].present? ? 'https' : 'http' }
-    end
+    # if request.domain == 'docuseal.com'
+    #   return { host: 'docuseal.com', protocol: ENV['FORCE_SSL'].present? ? 'https' : 'http' }
+    # end
 
     Docuseal.default_url_options
   end
